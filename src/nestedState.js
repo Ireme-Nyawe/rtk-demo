bd8e7a012f9d58cd73cbb20eb59a748f73d1df57
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-16 09:49:28 +0200

    add action creaters

--- src/nestedState.js
+++ src/nestedState.js
@@ -1,9 +1,15 @@
-const initialState={
-    name:"InOnO",
-    address:{
-        street:"kj",
-        city:"kg",
-        state:"rw"
-    }
-}
-const STREET_UPDATED='STREET_UPDATED'
+const initialState = {
+  name: "InOnO",
+  address: {
+    street: "kj",
+    city: "kg",
+    state: "rw",
+  },
+};
+const STREET_UPDATED = "STREET_UPDATED";
+const updateStreet = (street) => {
+  return {
+    type: STREET_UPDATED,
+    payload: street,
+  };
+};
