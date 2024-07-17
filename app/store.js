41b38935c0aa06ab0f6cb49d8ed623c0cacf5d8d
Ireme-Nyawe <akimana.inono@gmail.com>
2024-07-17 10:30:28 +0200

    ensure async actions with toolkit

--- app/store.js
+++ app/store.js
@@ -2,13 +2,15 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const iceReducer = require("../features/iceCream/iceCreamSlice");
+const userReducer = require("../features/user/userSlice");
 
 const logger = reduxLogger.createLogger();
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     iceCreame: iceReducer,
+    user: userReducer,
   },
-  middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 module.exports = store;
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const iceActions = require("./features/iceCream/iceCreamSlice").iceActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 console.log("initial state:", store.getState());
-const subs = store.subscribe(() => {});
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
-store.dispatch(iceActions.ordered());
-store.dispatch(iceActions.ordered());
-store.dispatch(iceActions.restocked(2));
+const subs = store.subscribe(() => {
+  console.log("updated", store.getState());
+});
+store.dispatch(fetchUsers());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+// store.dispatch(iceActions.ordered());
+// store.dispatch(iceActions.ordered());
+// store.dispatch(iceActions.restocked(2));
 
-subs();
+// subs();
